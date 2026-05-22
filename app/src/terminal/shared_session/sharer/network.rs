48ccd310c85a55f07f32b1dda248a5335f921eb6
Travis Bender <travisgbender@gmail.com>
2026-05-22 00:45:20 -0600

    Drop telemetry context from shared sessions
    Upstream-Base: ac6e8137
    
    Patch-Topic: drop-shared-session-telemetry-context

--- app/src/terminal/shared_session/sharer/network.rs
+++ app/src/terminal/shared_session/sharer/network.rs
@@ -52,8 +52,8 @@ use crate::throttle::throttle;
 
 #[cfg(not(any(test, feature = "integration_tests")))]
 use {
-    crate::{report_error, server::telemetry::telemetry_context},
-    session_sharing_protocol::common::{Scrollback, TelemetryContext},
+    crate::report_error,
+    session_sharing_protocol::common::Scrollback,
     session_sharing_protocol::sharer::SessionSourceType,
     session_sharing_protocol::sharer::{InitPayload, Lifetime},
 };
@@ -638,7 +638,7 @@ impl Network {
                         selection,
                         init_block_id: init_block_id.into(),
                         input_replica_id: input_replica_id.into(),
-                        telemetry_context: Some(TelemetryContext(telemetry_context().as_value())),
+                        telemetry_context: None,
                         universal_developer_input_context: Some(UniversalDeveloperInputContext {
                             selected_model: Some(SelectedAgentModel::new(selected_model_id)),
                             ..universal_developer_input_context
